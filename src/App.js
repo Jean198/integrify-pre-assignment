8c6b62a401b32bdedf0726541223a77022b9f9f1
Jean198 <jeandedieu.niyigaba@gmail.com>
2022-06-28 11:24:29 +0300

    just initialised the assignment project

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React, {useState} from 'react'
+import axios from 'axios'
+
+function App() {
+ 
+
+  return (
+    <div className="myApp">
+     <h1>This is my project</h1>
+        
+    </div>
+  );
+}
+
+export default App;
